6443eed115f26a86bcb7381d911e8fc7b48e6592
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2019-08-20 18:46:21 +0300

    Prettyfy testing output

--- app/Main.hs
+++ app/Main.hs
@@ -4,29 +4,32 @@ import GenericFeed
 
 import System.Environment
 
-import Data.Char
-import Data.List
 import Data.Maybe
 
 import Control.Monad
+import Control.Exception
 
-import Data.Text (Text)
-import qualified Data.Text as T
 import Data.ByteString.Lazy.Char8 (unpack)
 
 import Network.Wreq
+import Network.HTTP.Client (HttpException)
 import Control.Lens
 import Text.Feed.Import
 import Text.Feed.Types
-import qualified Text.Atom.Feed as A
-import qualified Text.RSS.Syntax as R
-import qualified Text.RSS1.Syntax as R1
 
 main :: IO ()
 main = do
-  [f] <- getArgs
-  us <- parseFeedsConfig <$> readFile f
-  feeds <- forM us $ \u -> do
-    x <- get u
-    pure $ fromJust $ parseFeedString $ unpack $ x ^. responseBody
-  putStrLn $ unlines $ map (showGenericFeed . feedToGeneric) feeds
+    [f] <- getArgs
+    us <- parseFeedsConfig <$> readFile f
+    feeds <- forM us $ \u -> do
+      putStr $ u ++ "... "
+      flip catch handler $ do
+        x <- get u
+        putStrLn "ok"
+        pure $ parseFeedString $ unpack $ x ^. responseBody
+    putStrLn $ unlines $ map (showGenericFeed . feedToGeneric) $ catMaybes feeds
+  where
+    handler :: HttpException -> IO (Maybe Feed)
+    handler _ = do
+      putStrLn "HttpException"
+      pure Nothing
