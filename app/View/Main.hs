f7eaddf22761a2f1354b5cb8b627d42114606a3b
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2019-08-20 22:58:58 +0300

    Add another level for the menu

--- app/View/Main.hs
+++ app/View/Main.hs
@@ -5,6 +5,7 @@ module Main where
 
 import System.Environment
 
+import Data.Maybe
 import Data.List
 import qualified Data.Text as T
 
@@ -21,24 +22,45 @@ instance Splittable [] where
 
 type MyList n e = GenericList n [] e
 
+toGenericList :: [a] -> MyList () a
+toGenericList x = list () x 1
+
 data State = State
   { sFeeds :: MyList () GenericFeed
   , sItems :: Maybe (MyList () GenericItem)
   }
 
+renderItem :: Bool -> GenericItem -> Widget ()
+renderItem _ (GenericItem {..}) = txt $ fromMaybe "*Empty*" giTitle
+
 renderFeed :: Bool -> GenericFeed -> Widget ()
 renderFeed _ (GenericFeed {..}) = txt $ T.pack gfTitle <> " (" <> gfURL <> ")"
 
 draw :: State -> [Widget ()]
-draw (State {..}) = [vCenter $ vBox [hCenter $ renderList renderFeed True sFeeds, str "", hCenter $ str "Press Q/Esc to quit"]]
+draw (State {..}) = [vCenter $ vBox [hCenter $ l, str "", hCenter $ str "Press Q to quit, Esc to go back"]]
+  where
+    l = case sItems of
+      Nothing -> renderList renderFeed True sFeeds
+      Just is -> renderList renderItem True is
 
--- EvKey (KChar 'q') []
 handle :: State -> BrickEvent () () -> EventM () (Next State)
-handle s (VtyEvent (EvKey (KChar 'q') _)) = halt s
-handle s (VtyEvent (EvKey KEsc _)) = halt s
-handle s (VtyEvent e) = do
-  l <- handleListEventVi handleListEvent e (sFeeds s)
-  continue $ s { sFeeds = l }
+handle s@(State {..}) (VtyEvent (EvKey (KChar 'q') _)) =
+  case sItems of
+    Nothing -> halt s
+    Just _ -> continue $ s { sItems = Nothing }
+handle s@(State {..}) (VtyEvent (EvKey KEnter _)) =
+  case sItems of
+    Nothing -> continue
+      $ s { sItems = Just $ toGenericList $ gfItems $ snd $ fromJust $ listSelectedElement sFeeds }
+    Just _ -> continue s
+handle s@(State {..}) (VtyEvent e) =
+  case sItems of
+    Nothing -> do
+      l <- handleListEventVi handleListEvent e sFeeds
+      continue $ s { sFeeds = l }
+    Just is -> do
+      l <- handleListEventVi handleListEvent e is
+      continue $ s { sItems = Just l }
 handle s _ = continue s
 
 theMap :: AttrMap
@@ -60,5 +82,5 @@ main :: IO ()
 main = do
   [cFile] <- getArgs
   feeds <- (read :: String -> [GenericFeed]) <$> readFile cFile
-  let s = State (list () feeds 1) undefined
+  let s = State (toGenericList feeds) Nothing
   pure () <* defaultMain app s
