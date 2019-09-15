d79441e176294364e004c5ae17f356ce505de46e
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2019-09-15 22:19:35 +0200

    Final refactoring of interenal structures

--- app/View/Main.hs
+++ app/View/Main.hs
@@ -22,19 +22,33 @@ instance Splittable [] where
   splitAt = Data.List.splitAt
 
 type MyList n e = GenericList n [] e
+type L e = MyList () e
 
 toGenericList :: [a] -> MyList () a
 toGenericList x = list () x 1
 
-data MenuPosition
-  = MenuFeeds
-  | MenuItems (MyList () (GenericItem, ItemStatus))
-  | MenuContents (MyList () (GenericItem, ItemStatus)) GenericItem
+-- Internal state of the interface and a couple of itw utility functions
 
-data State = State
-  { sFeeds :: MyList () (String, Maybe CacheEntry)
-  , sMenu  :: MenuPosition
-  }
+data State
+  = LevelFeeds    (L (String, Maybe CacheEntry))
+  | LevelItems    (L (String, Maybe CacheEntry)) (String, Maybe CacheEntry) (L (GenericItem, ItemStatus))
+  | LevelContents (L (String, Maybe CacheEntry)) (String, Maybe CacheEntry) (L (GenericItem, ItemStatus)) (GenericItem, ItemStatus)
+
+stateDown :: State -> State
+stateDown s@(LevelFeeds fs) =
+  let f = snd $ fromJust $ listSelectedElement fs
+  in case snd f of
+    Nothing -> s
+    Just (_, is) -> LevelItems fs f $ toGenericList is
+stateDown (LevelItems fs f is) =
+  let i = snd $ fromJust $ listSelectedElement is
+  in LevelContents fs f is i
+stateDown s@(LevelContents _ _ _ _) = s
+
+stateUp :: State -> State
+stateUp s@(LevelFeeds _) = s
+stateUp (LevelItems fs _ _) = LevelFeeds fs
+stateUp (LevelContents fs f is _) = LevelItems fs f is
 
 renderContents :: GenericItem -> Widget ()
 renderContents (GenericItem {..}) =
@@ -57,39 +71,30 @@ renderCache b (_, Just (f, _)) = renderFeed b f
 renderCache _ (s, Nothing) = txt $ T.pack s <> " *Not Fetched*"
 
 draw :: State -> [Widget ()]
-draw (State {..}) =
-    case sMenu of
-      MenuFeeds -> g $ renderList renderCache True sFeeds
-      MenuItems is -> g $ renderList renderItem True is
-      MenuContents _ c -> [f $ padBottom Max $ renderContents c]
+draw s =
+    case s of
+      LevelFeeds fs -> g $ renderList renderCache True fs
+      LevelItems _ _ is -> g $ renderList renderItem True is
+      LevelContents _ _ _ (c, _) -> [f $ padBottom Max $ renderContents c]
   where
     f x = vBox [hCenter $ x, str "", hCenter $ str "Press Q to go back or quit"]
     g x = [vCenter $ f x]
 
 handle :: State -> BrickEvent () () -> EventM () (Next State)
-handle s@(State {..}) (VtyEvent (EvKey (KChar 'q') _)) =
-  case sMenu of
-    MenuFeeds -> halt s
-    MenuItems _ -> continue $ s { sMenu = MenuFeeds }
-    MenuContents is _ -> continue $ s { sMenu = MenuItems is }
-handle s@(State {..}) (VtyEvent (EvKey KEnter _)) =
-  case sMenu of
-    MenuFeeds -> continue
-      $ case snd $ snd $ fromJust $ listSelectedElement sFeeds of
-          Nothing -> s
-          Just (_, is) -> s { sMenu = MenuItems $ toGenericList is}
-    MenuItems is -> continue
-      $ s { sMenu = MenuContents is $ fst $ snd $ fromJust $ listSelectedElement is }
-    MenuContents _ _ -> continue s
-handle s@(State {..}) (VtyEvent e) =
-  case sMenu of
-    MenuFeeds -> do
-      l <- handleListEventVi handleListEvent e sFeeds
-      continue $ s { sFeeds = l }
-    MenuItems is -> do
-      l <- handleListEventVi handleListEvent e is
-      continue $ s { sMenu = MenuItems l }
-    MenuContents _ _ -> continue s
+handle s (VtyEvent (EvKey (KChar 'q') _)) =
+  case s of
+    LevelFeeds _ -> halt s
+    _ -> continue $ stateUp s
+handle s (VtyEvent (EvKey KEnter _)) = continue $ stateDown s
+handle s (VtyEvent e) =
+  case s of
+    LevelFeeds fs -> do
+      fs' <- handleListEventVi handleListEvent e fs
+      continue $ LevelFeeds fs'
+    LevelItems fs f is -> do
+      is' <- handleListEventVi handleListEvent e is
+      continue $ LevelItems fs f is'
+    LevelContents _ _ _ _ -> continue s
 handle s _ = continue s
 
 theMap :: AttrMap
@@ -111,5 +116,5 @@ main :: IO ()
 main = do
   [cFile] <- getArgs
   feeds <- (read :: String -> [(String, Maybe CacheEntry)]) <$> readFile cFile
-  let s = State (toGenericList feeds) MenuFeeds
+  let s = LevelFeeds (toGenericList feeds)
   pure () <* defaultMain app s
