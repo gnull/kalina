6b55d4a0b5182026a96ebf7de38f1c01c8013271
Ivan Oleynikov <ivanol@chalmers.se>
2019-10-07 18:17:38 +0200

    Move Menu-related code to a separate module

--- app/View/Main.hs
+++ app/View/Main.hs
@@ -3,109 +3,32 @@
 
 module Main where
 
-import Data.Maybe
-import Data.List
 import Data.Text () -- Instances
-import qualified Data.Text as T
-import Control.Monad.IO.Class (MonadIO(..))
 
 import GenericFeed
 
 import Brick
 import Brick.Widgets.List
-import Brick.Widgets.Center
-import Graphics.Vty.Input.Events
 import qualified Graphics.Vty as V
 
 import Options.Applicative hiding (str)
 
-instance Splittable [] where
-  splitAt = Data.List.splitAt
+import Menu
 
-type MyList n e = GenericList n [] e
-type L e = MyList () e
+data State = State MenuState
 
-toGenericList :: [a] -> MyList () a
-toGenericList x = list () x 1
+cacheFromState :: State -> CacheFile
+cacheFromState = undefined
 
-
--- Internal state of the interface and a couple of itw utility functions
-
-data State
-  = LevelFeeds    (L (String, Maybe CacheEntry))
-  | LevelItems    (L (String, Maybe CacheEntry)) (String, Maybe CacheEntry) (L (GenericItem, ItemStatus))
-  | LevelContents (L (String, Maybe CacheEntry)) (String, Maybe CacheEntry) (L (GenericItem, ItemStatus)) (GenericItem, ItemStatus)
-
-stateDown :: State -> State
-stateDown s@(LevelFeeds fs) =
-  let f = snd $ fromJust $ listSelectedElement fs
-  in case snd f of
-    Nothing -> s
-    Just (_, is) -> LevelItems fs f $ toGenericList is
-stateDown (LevelItems fs f is) =
-  let i = snd $ fromJust $ listSelectedElement is
-  in LevelContents fs f is i
-stateDown s@(LevelContents _ _ _ _) = s
-
-stateUp :: State -> State
-stateUp s@(LevelFeeds _) = s
-stateUp (LevelItems fs _ _) = LevelFeeds fs
-stateUp (LevelContents fs f is _) = LevelItems fs f is
-
-renderContents :: GenericItem -> Widget ()
-renderContents (GenericItem {..}) =
-  txtWrap $ T.unlines $ catMaybes
-    [ ("Title: " <>) <$> giTitle
-    , ("Link: " <>) <$> giURL
-    , ("Author: " <>) <$> giAuthor
-    , Just ""
-    , giBody
-    ]
-
-renderItem :: Bool -> (GenericItem , ItemStatus)-> Widget ()
-renderItem _ (GenericItem {..}, _) = padRight Max $ txt $ fromMaybe "*No Date*" giDate <> "  " <> (T.unwords $ T.words $ fromMaybe "*Empty*" giTitle)
-
-renderFeed :: Bool -> GenericFeed -> Widget ()
-renderFeed _ (GenericFeed {..}) = padRight Max $ txt $ T.pack gfTitle <> " (" <> gfURL <> ")"
-
-renderCache :: Bool -> (String, Maybe CacheEntry) -> Widget ()
-renderCache b (_, Just (f, _)) = renderFeed b f
-renderCache _ (s, Nothing) = padRight Max $ txt $ T.pack s <> " *Not Fetched*"
+initialState :: CacheFile -> State
+initialState = State . initialMenuState
 
 draw :: State -> [Widget ()]
-draw s =
-    case s of
-      LevelFeeds fs -> g $ renderList renderCache True fs
-      LevelItems _ _ is -> g $ renderList renderItem True is
-      LevelContents _ _ _ (c, _) -> [f $ padBottom Max $ renderContents c]
-  where
-    f x = vBox [hCenter $ x, str "", hCenter $ str "Press Q to go back or quit"]
-    g x = [vCenter $ f x]
+draw (State s) = [drawMenu s]
 
-handle :: (CacheFile -> IO ()) -> State -> BrickEvent () () -> EventM () (Next State)
-handle _ s (VtyEvent (EvKey (KChar 'r') _)) =
-  case s of
-    LevelFeeds fs -> do
-      let (u, c) = snd $ fromJust $ listSelectedElement fs
-      (f, is) <- liftIO $ fetchFeed u
-      let c' = Just $ fromMaybe (newCacheEntry f is) $ fmap (updateCacheEntry f is) c
-      continue $ LevelFeeds $ listModify (const (u, c')) fs
-    _ -> continue s
-handle _ s (VtyEvent (EvKey (KChar 'q') _)) =
-  case s of
-    LevelFeeds _ -> halt s
-    _ -> continue $ stateUp s
-handle _ s (VtyEvent (EvKey KEnter _)) = continue $ stateDown s
-handle _ s (VtyEvent e) =
-  case s of
-    LevelFeeds fs -> do
-      fs' <- handleListEventVi handleListEvent e fs
-      continue $ LevelFeeds fs'
-    LevelItems fs f is -> do
-      is' <- handleListEventVi handleListEvent e is
-      continue $ LevelItems fs f is'
-    LevelContents _ _ _ _ -> continue s
-handle _ s _ = continue s
+handle :: State -> BrickEvent () () -> EventM () (Next State)
+handle (State s) (VtyEvent e) = fmap State <$> handleMenu s e
+handle s _ = continue s
 
 theMap :: AttrMap
 theMap = attrMap V.defAttr
@@ -113,10 +36,10 @@ theMap = attrMap V.defAttr
     , (listSelectedAttr,    V.white `on` V.blue)
     ]
 
-app :: (CacheFile -> IO ()) -> App State () ()
-app f = App
+app :: App State () ()
+app = App
   { appDraw = draw
-  , appHandleEvent = handle f
+  , appHandleEvent = handle
   , appStartEvent = return
   , appAttrMap = const $ theMap
   , appChooseCursor = neverShowCursor
@@ -157,7 +80,5 @@ main = do
   Options {..} <- parseOpts
   urls <- parseFeedsConfig <$> readFile oUrls
   feeds <- refreshCacheFileWithUrls urls <$> readCacheFile oCache
-  let saveCache = writeCacheFile oCache
-  saveCache feeds
-  let s = LevelFeeds (toGenericList feeds)
-  pure () <* defaultMain (app saveCache) s
+  let s = initialState feeds
+  writeCacheFile oCache =<< cacheFromState <$> defaultMain app s
---
+++ src/Menu.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Menu where
+
+import Data.Maybe
+import Data.List
+import Data.Text () -- Instances
+import qualified Data.Text as T
+import Control.Monad.IO.Class (MonadIO(..))
+
+import GenericFeed
+
+import Brick
+import Brick.Widgets.List
+import Brick.Widgets.Center
+import Graphics.Vty.Input.Events
+
+instance Splittable [] where
+  splitAt = Data.List.splitAt
+
+type MyList n e = GenericList n [] e
+type L e = MyList () e
+
+toGenericList :: [a] -> MyList () a
+toGenericList x = list () x 1
+
+data MenuState
+  = LevelFeeds    (L (String, Maybe CacheEntry))
+  | LevelItems    (L (String, Maybe CacheEntry)) (String, Maybe CacheEntry) (L (GenericItem, ItemStatus))
+  | LevelContents (L (String, Maybe CacheEntry)) (String, Maybe CacheEntry) (L (GenericItem, ItemStatus)) (GenericItem, ItemStatus)
+
+initialMenuState :: CacheFile -> MenuState
+initialMenuState = LevelFeeds . toGenericList
+
+stateDown :: MenuState -> MenuState
+stateDown s@(LevelFeeds fs) =
+  let f = snd $ fromJust $ listSelectedElement fs
+  in case snd f of
+    Nothing -> s
+    Just (_, is) -> LevelItems fs f $ toGenericList is
+stateDown (LevelItems fs f is) =
+  let i = snd $ fromJust $ listSelectedElement is
+  in LevelContents fs f is i
+stateDown s@(LevelContents _ _ _ _) = s
+
+stateUp :: MenuState -> MenuState
+stateUp s@(LevelFeeds _) = s
+stateUp (LevelItems fs _ _) = LevelFeeds fs
+stateUp (LevelContents fs f is _) = LevelItems fs f is
+
+renderContents :: GenericItem -> Widget ()
+renderContents (GenericItem {..}) =
+  txtWrap $ T.unlines $ catMaybes
+    [ ("Title: " <>) <$> giTitle
+    , ("Link: " <>) <$> giURL
+    , ("Author: " <>) <$> giAuthor
+    , Just ""
+    , giBody
+    ]
+
+renderItem :: Bool -> (GenericItem , ItemStatus)-> Widget ()
+renderItem _ (GenericItem {..}, _) = padRight Max $ txt $ fromMaybe "*No Date*" giDate <> "  " <> (T.unwords $ T.words $ fromMaybe "*Empty*" giTitle)
+
+renderFeed :: Bool -> GenericFeed -> Widget ()
+renderFeed _ (GenericFeed {..}) = padRight Max $ txt $ T.pack gfTitle <> " (" <> gfURL <> ")"
+
+renderCache :: Bool -> (String, Maybe CacheEntry) -> Widget ()
+renderCache b (_, Just (f, _)) = renderFeed b f
+renderCache _ (s, Nothing) = padRight Max $ txt $ T.pack s <> " *Not Fetched*"
+
+drawMenu :: MenuState -> Widget ()
+drawMenu s =
+    case s of
+      LevelFeeds fs -> g $ renderList renderCache True fs
+      LevelItems _ _ is -> g $ renderList renderItem True is
+      LevelContents _ _ _ (c, _) -> f $ padBottom Max $ renderContents c
+  where
+    f x = vBox [hCenter $ x, str "", hCenter $ str "Press Q to go back or quit"]
+    g x = vCenter $ f x
+
+handleMenu :: MenuState -> Event -> EventM () (Next MenuState)
+handleMenu s (EvKey (KChar 'r') _) =
+  case s of
+    LevelFeeds fs -> do
+      let (u, c) = snd $ fromJust $ listSelectedElement fs
+      (f, is) <- liftIO $ fetchFeed u
+      let c' = Just $ fromMaybe (newCacheEntry f is) $ fmap (updateCacheEntry f is) c
+      continue $ LevelFeeds $ listModify (const (u, c')) fs
+    _ -> continue s
+handleMenu s (EvKey (KChar 'q') _) =
+  case s of
+    LevelFeeds _ -> halt s
+    _ -> continue $ stateUp s
+handleMenu s (EvKey KEnter _) = continue $ stateDown s
+handleMenu s e =
+  case s of
+    LevelFeeds fs -> do
+      fs' <- handleListEventVi handleListEvent e fs
+      continue $ LevelFeeds fs'
+    LevelItems fs f is -> do
+      is' <- handleListEventVi handleListEvent e is
+      continue $ LevelItems fs f is'
+    LevelContents _ _ _ _ -> continue s
