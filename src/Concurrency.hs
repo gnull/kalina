95efd7c1757450b5ce58b81c8e55ee240893f5f1
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2020-02-28 16:19:26 +0100

    Remove unused function

--- src/Concurrency.hs
+++ src/Concurrency.hs
@@ -46,9 +46,6 @@ patch (f, is) Nothing = Just $ newCacheEntry f is
 patch (f, is) (Just (f', is')) = Just (f, goodIs ++ is')
   where goodIs = map (,False) $ foldr Data.List.delete is $ map fst is'
 
-patchList :: L (String, Maybe CacheEntry) -> (String, (GenericFeed, [GenericItem])) -> L (String, Maybe CacheEntry)
-patchList fs (u, f) = fs <&> \(u', f') -> if u == u' then (u, patch f f') else (u', f')
-
 handleThreadEvent :: CacheFile -> WorkerEvent -> CacheFile
 handleThreadEvent s (Finished u f) = map g s
   where g (u', f') = (u', if u' == u then patch f f' else f')
